17512fe271084a93e86ea7f11edf4190ccb3561f
Tornike Tsagareishvili <dht5152@gmail.com>
2024-10-16 19:40:21 +0400

    Add: Unique Ids For All Users

--- Models/UsersModel.js
+++ Models/UsersModel.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const UsersSchema = new mongoose.Schema({
   username: String,
   userImage: String,
+  id: String,
 });
 
 const UsersModel = mongoose.model("Users", UsersSchema);
--- Routes/ReceiveUser.js
+++ Routes/ReceiveUser.js
@@ -1,12 +1,17 @@
 import express from "express";
 import UsersModel from "../Models/UsersModel.js";
+import { v4 as uuidv4 } from "uuid";
 
 const receiveUser = express.Router();
 
 receiveUser.post("/user", async (req, res) => {
   const { name, image } = req.body;
 
-  const newUser = new UsersModel({ username: name, userImage: image });
+  const newUser = new UsersModel({
+    username: name,
+    userImage: image,
+    id: uuidv4(),
+  });
   await newUser.save();
 
   res.status(201).send(newUser);
