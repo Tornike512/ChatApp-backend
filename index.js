dcd7e744af4dd362052df549baf52bc72b271b19
Tornike Tsagareishvili <dht5152@gmail.com>
2024-10-16 22:41:49 +0400

    Add: Socket.io Basic Setup In Serve

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import cors from "cors";
 import receiveUser from "./Routes/ReceiveUser.js";
 import SendUserData from "./Routes/SendUserData.js";
 import SendAllUsers from "./Routes/SendAllUsers.js";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 dotenv.config();
 
@@ -12,8 +14,12 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+const server = createServer(app);
+
 app.use(express.json());
 app.use(cors());
+
+const io = new Server(server);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("Connected to MongoDB");
@@ -22,6 +28,14 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 app.use(receiveUser);
 app.use(SendUserData);
 app.use(SendAllUsers);
+
+io.on("connection", (socket) => {
+  socket.emit("Send message");
+
+  socket.on("receive message", (arg) => {
+    console.log(arg);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
