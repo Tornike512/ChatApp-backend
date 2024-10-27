231531957bd01d6b7cdcc8e2f40b6146b5d90a2a
Tornike Tsagareishvili <dht5152@gmail.com>
2024-10-27 15:51:46 +0400

    Fix: Making UsersModel Required False

--- Models/UsersModel.js
+++ Models/UsersModel.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const UsersSchema = new mongoose.Schema({
-  username: String,
-  userImage: String,
-  id: String,
+  username: { type: String, required: false },
+  userImage: { type: String, required: false },
+  id: { type: String, required: false },
 });
 
 const UsersModel = mongoose.model("Users", UsersSchema);
--- Routes/SendAllUsers.js
+++ Routes/SendAllUsers.js
@@ -4,7 +4,7 @@ import UsersModel from "../Models/UsersModel.js";
 const SendAllUsers = express.Router();
 
 SendAllUsers.get("/all-users", async (req, res) => {
-  const allUsers = await UsersModel.find({}, "username userImage");
+  const allUsers = await UsersModel.find({}, "username");
 
   res.status(200).send(allUsers);
 });
